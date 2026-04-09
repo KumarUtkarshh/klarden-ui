42485c145f5ef43d0e96beb1c36e25bb0130834c
devolos <utk27032005@gmail.com>
2026-04-09 17:49:56 +0530

    mc

--- registry/klarden-ui/pagination/index.ts
+++ registry/klarden-ui/pagination/index.ts
@@ -1,4 +1,4 @@
 export { Pagination } from "./pagination";
-export { usePaginationState } from "./use-pagination-state";
-export type { PaginationColor, PaginationVariant } from "./types";
 export type { PaginationProps } from "./pagination";
+export type { PaginationColor, PaginationVariant } from "./types";
+export { usePaginationState } from "./use-pagination-state";
